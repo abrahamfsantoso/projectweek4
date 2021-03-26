4831b0d92b7fa0b0f783bf4f06fccc3986356f83
Fikri Lubis <fikriaplubis@gmail.com>
2021-03-26 15:14:30 +0700

    Add barang

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express(); //create express app
 
 //import routes
 const transaksiRoutes = require("./routes/transaksiRoutes");
-const pemasokController = require("./routes/pemasokRoutes");
+const pemasokRoutes = require("./routes/pemasokRoutes");
 const barangRoutes = require("./routes/barangRoutes");
 app.use(express.urlencoded({ extended: true })); // used to read req.body
 
 //define route
 app.use("/transaksi", transaksiRoutes);
-app.use("/pemasok", pemasokController);
+app.use("/pemasok", pemasokRoutes);
 app.use("/barang", barangRoutes);
 app.listen(3000, () => console.log("nyala at 3000!")); //running on port 3000
